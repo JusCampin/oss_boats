3ec96862e01580c7a4338dc18a08a256eff02fa0
JusCampin <37031311+JusCampin@users.noreply.github.com>
2023-01-05 23:32:59 -0800

    added ox libs

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,9 +5,14 @@ game 'rdr3'
 lua54 'yes'
 
 
-client_scripts { 'client/client.lua' }
+client_scripts {
+    'client/client.lua'
+}
 
-server_scripts { 'server/server.lua' }
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/server.lua'
+}
 
 shared_scripts {
     'config.lua',
